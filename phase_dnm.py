3a4ada44a1dae4ca7ff3c2a0691020d7582c9e34
Queen Jobo <j.kaplanis@gmail.com>
2017-07-18 16:56:50 +0100

    working gts

--- phase_dnm.py
+++ phase_dnm.py
@@ -6,7 +6,7 @@ Wrapper script to use DNG phaser from trio VCF
 
 python phase_dnm.py --vcf VCF [--probandid PROBANDID] [--motherid MOTHERID] [--fatherid FATHERID] [--parseoutput]
 
-python phase_dnm.py --vcf /lustre/scratch115/projects/ddd/users/jk18/mutatpheno/triovcfs/DDDP101263_wgs_trio.recode.vcf --probandid DDD_MAIN6529016 --motherid DDD_MAIN6529046 --fatherid DDD_MAIN6529041
+python /nfs/users/nfs_j/jk18/PhD/DeNovoPhaser/phase_dnm.py --vcf /lustre/scratch115/projects/ddd/users/jk18/mutatpheno/triovcfs/DDDP101263_wgs_trio.recode.vcf --dnmfile /lustre/scratch115/projects/ddd/users/jk18/mutatpheno/DNG/DDD_MAIN6529016_dnm.tab --probandbam /lustre/scratch115/projects/ddd/users/jk18/mutatpheno/bams/ROI__windel_DDDP101263_proband.bam --probandid DDD_MAIN6529016 --motherid DDD_MAIN6529046 --fatherid DDD_MAIN6529041
 '''
 
 #---------------IMPORTS---------------------
@@ -14,12 +14,15 @@ python phase_dnm.py --vcf /lustre/scratch115/projects/ddd/users/jk18/mutatpheno/
 import vcf
 import argparse
 
+DNG_PATH = "/software/ddd/internal/dng-pipeline/denovogear-0.5.4_wtsi_sc13_no_limits/bin/denovogear"
 #---------------FUNCTIONS-------------------
 
 def get_options():
     '''parse command line options'''
     parser = argparse.ArgumentParser()
     parser.add_argument('--vcf',type = str,help = 'path to multisample VCF/ trio vcf',required = True)
+    parser.add_argument('--probandbam',type = str, help = 'path to bam for proband',required = True)
+    parser.add_argument('--dnmfile', type = str, help = 'dnms file', required = True)
     parser.add_argument('--probandid',type = str,help = 'sample id for proband in VCF',required = False,default = "")
     parser.add_argument('--fatherid',type = str,help = 'sample id for father in VCF',required = False,default = "")
     parser.add_argument('--motherid',type = str,help = 'sample id for mother in VCF',required = False,default = "")
@@ -28,6 +31,7 @@ def get_options():
     return(args)
 
 def get_ids(pid,fid,mid):
+    '''get ids from parsing input otherwise assume vcf is in correct order'''
     if len(pid)+len(fid)+len(mid) == 0:
         print("Assuming samples are in order proband,father,mother in VCF")
         vcf_reader = vcf.Reader(filename=myvcf,compressed=True)
@@ -40,6 +44,7 @@ def get_ids(pid,fid,mid):
     return(pid,fid,mid)
 
 def get_geno(record,id):
+    '''get genotype in form of AA'''
     call = record.genotype(id)
     bases = call.gt_bases
     if bases is not None and len(bases) == 3:
@@ -50,8 +55,8 @@ def get_geno(record,id):
 
 def make_gts(myvcf,pid,fid,mid):
     '''make gts file given vcf and ids of samples in vcf'''
-    print("Making gts file, writing to "+outputfile)
     outputfile = pid + "_gts.tab"
+    print("Making gts file, writing to "+outputfile)
     with open(outputfile,'w') as f:
         vcf_reader = vcf.Reader(filename=myvcf,compressed=False)
         for record in vcf_reader:
@@ -62,16 +67,21 @@ def make_gts(myvcf,pid,fid,mid):
             mgeno = get_geno(record,mid)
             if pgeno is None or fgeno is None or mgeno is None:
                 continue
-            myline = "\t".join([chrm,str(pos),pgeno,fgeno,mgeno])
+            myline = "\t".join([chrm,str(pos),pgeno,fgeno,mgeno])+"\n"
             f.write(myline)
-    return(True)
+    return(outputfile)
 
-def run_dng_phase(
+def run_dng_phase(probandid,dnmfile,gtsfile,probandbam):
+    print("submitting dng phase")
+    outfile = probandid + "_dnms_phased.dnm"
+    myc = DNG_PATH + " phaser --dnm " + dnmfile + " --pgt " + gtsfile + " --bam " + probandbam + " --window 1000 > " + outfile 
+    print(myc)
 
 def main():
     args = get_options()
     pid,fid,mid = get_ids(args.probandid,args.fatherid,args.motherid)
-    done = make_gts(args.vcf,pid,fid,mid)
+    gtsfile = make_gts(args.vcf,pid,fid,mid)
+    run_dng_phase(args.probandid,args.dnmfile,gtsfile,args.probandbam)
     
     
     
